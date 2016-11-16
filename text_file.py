d7a092a0a686fd2caa82f53e83171cb10590596b
Dmytro Tkachuk <dimatkachukgf@gmail.com>
2016-11-16 23:53:08 +0200

    Add files via upload

--- text_file.py
+++ text_file.py
@@ -1,42 +1,44 @@
 class File:
 
     def __init__(self):
-        self.rows = [""]
+        self.rows = []
 
     def __init__(self, string_list):
         self.rows = []
         for elem in string_list:
             self.rows.append(elem)
 
-    def insert(self, i, j, char):
-        if len(self.rows[i]) < 100:
-            self.rows[i] = self.rows[i][:j] + char + self.rows[i][j:]
+    def change(self, i, j, char):
+        last_column = len(self.rows) - 1
+        if char == "bs":
+            if len(self.rows[i]):
+                self.rows[i] = self.rows[i][:j-1] + self.rows[i][j:]
+            else:
+                self.rows.pop(i)
+        elif char == "ent":
+            self.rows.insert(i+1,"")
+            if self.rows[i][:j]:
+                self.rows[i+1] = self.rows[i][j:]
+                self.rows[i] = self.rows[i][:j]
         else:
             self.rows[i] = self.rows[i][:j] + char + self.rows[i][j:]
-            tail = self.rows[i][100]
-            for k in range(i+1,len(self.rows)):
-                self.rows[k] = tail + self.rows[k]
-                if len(self.rows[k]) == 101:
-                    tail = self.rows[k][100]
-                    if k == len(self.rows) - 1:
-                        self.rows.append("")
-                        self.rows[-1] = tail + self.rows[-1]
-                else:
-                    break
+
 
 
 
 
 if __name__ == "__main__":
-    s1 = "a" * 100
-    s2 = "q" * 100
-    s3 = "w" * 100
+    s1 = "a" * 3
+    s2 = "qwqwqwqwqw"
+    s3 = ""
+    s4 = "asdf"
     l = []
     l.append(s1)
     l.append(s2)
     l.append(s3)
+    l.append(s4)
     f = File(l)
-    f.insert(0,12,"b")
+    f.change(1,3,"ent")
     for elem in f.rows:
         print elem
 
